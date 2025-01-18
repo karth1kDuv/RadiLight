76ccfadf24aadba1e49ebd74bd3bf4776e46aec1
KDKarthikDuvvuri <karthik.duvvuri08@gmail.com>
2025-01-18 00:36:07 -0600

    idk man queue shit

--- main.cu
+++ main.cu
@@ -10,19 +10,12 @@
 // Standard library and utility includes
 #include <cmath> 
 #include <iostream>
-#include <fstream>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <atomic>
 #include <chrono>
 #include <thread>
+#include <condition_variable>
 #include <mutex>
+#include <queue>
 
 
 struct AprilTagsImpl {
@@ -108,7 +101,7 @@ void roborioSender(const std::vector<std::array<float, 8>> &sendData, int sock) 
         return;
     }
 
-    sockaddr_in destination = {AF_INET, htons(9000), inet_addr("192.168.86.239")};
+    sockaddr_in destination = {AF_INET, htons(9000), inet_addr("10.75.3.2")};
     size_t size = sendData.size() * sizeof(std::array<float, 8>);
 
     int n_bytes = ::sendto(sock, sendData.data(), size, 0, 
@@ -190,124 +183,126 @@ std::array<float, 8> getPose(std::vector<cv::Point2d> imagePts, int TagId){
 }
 
 
-std::atomic<bool> processing_done(true);
-std::mutex frame_mutex;
 cv::VideoCapture capture;
-cv::Mat frame, img_rgba8;
+cv::Mat frame;
+std::queue<cv::Mat> frame_queue;
+std::mutex frame_queue_mutex;
+std::condition_variable frame_queue_cond;
 
 void captureThread() {
+    const size_t MAX_QUEUE_SIZE = 30;
+
     while (capture.isOpened()) {
         cv::Mat new_frame;
         capture >> new_frame;
         if (new_frame.empty()) {
             break;
         }
-        std::lock_guard<std::mutex> lock(frame_mutex);
-        frame = new_frame;
-        processing_done = false;
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        std::unique_lock<std::mutex> lock(frame_queue_mutex);
+        if (frame_queue.size() >= MAX_QUEUE_SIZE) {
+            frame_queue.pop();
+        }
+
+       
+        frame_queue.push(new_frame);
+        frame_queue_cond.notify_one();
+
+        lock.unlock();
     }
 }
 
+
 void processingThread(AprilTagsImpl *impl_, int roborioSock) { 
-    std::vector<cv::Point2d> imagePts;
     std::vector<std::array<float, 8>> finalToSend;
-
-    while (capture.isOpened()) {
-        auto start = std::chrono::steady_clock::now(); 
-
-        if (!processing_done) {
-            cv::Mat local_frame;
-            {
-                std::lock_guard<std::mutex> lock(frame_mutex);
-                if (frame.empty()) {
-                    processing_done = true;
-                    continue;
-                }
-                frame.copyTo(local_frame);
-                //cv::resize(local_frame, local_frame, cv::Size(local_frame.cols / 2, local_frame.rows / 2), 0, 0, cv::INTER_LINEAR);
-                cv::cvtColor(local_frame, img_rgba8, cv::COLOR_BGR2RGBA);
-            }
-            auto frame_end = std::chrono::steady_clock::now();
-            
-            auto memory_start = std::chrono::steady_clock::now();
-
-            const cudaError_t cuda_error =
-                cudaMemcpyAsync(impl_->input_image_buffer, (uchar4 *)img_rgba8.ptr<unsigned char>(0),
-                impl_->input_image_buffer_size, cudaMemcpyHostToDevice, impl_->main_stream);
-
-            if (cuda_error != cudaSuccess) {
-                throw std::runtime_error(
-                        "Could not memcpy to device CUDA memory (error code " +
-                        std::to_string(cuda_error) + ")");
-            }
-            auto memory_end = std::chrono::steady_clock::now();
-            
-            
-            auto detect_start = std::chrono::steady_clock::now();
-            uint32_t numDetections;
-            const int error = nvAprilTagsDetect(
-                    impl_->april_tags_handle, &(impl_->input_image), impl_->tags.data(),
-                    &numDetections, impl_->max_tags, impl_->main_stream);
-            if (error != 0) {
-                throw std::runtime_error("Failed to run AprilTags detector (error code " +
-                                        std::to_string(error) + ")");
-            }
-
-            
-
-            auto detect_end = std::chrono::steady_clock::now();
-
-            
-            auto getPose_start = std::chrono::steady_clock::now();
-
-            std::cout << numDetections;
-            std::cout << std::endl;
-
-            for (int i = 0; i < numDetections; i++) {
-                const nvAprilTagsID_t &detection = impl_->tags[i];
-                for (auto corner : detection.corners) {
-                    imagePts.push_back(cv::Point2d(corner.x, corner.y));
-                    cv::circle(frame, cv::Point(corner.x, corner.y), 3, cv::Scalar(255, 255, 255));
-                }
-                finalToSend.push_back(getPose(imagePts, detection.id));
-                imagePts.clear();
-            }
-
-            for (const auto &pose : finalToSend) {
-                std::cout << "Tag ID: " << pose[7] << " | Pose: ";
-                for (size_t i = 0; i < 7; i++) {
-                    std::cout << pose[i] << " ";
-                }
-                std::cout << std::endl;
-            }
-            
-            roborioSender(finalToSend, roborioSock);
-            auto getPose_end = std::chrono::steady_clock::now();
-
-            // Total time for frame processing
-            auto end = std::chrono::steady_clock::now();
-            std::chrono::duration<double> frame_time = end - start;
-            
-            std::chrono::duration<double> get_frame_time = frame_end - start;
-            std::chrono::duration<double> memory_time = memory_end - memory_start;
-            std::chrono::duration<double> detect_time = detect_end - detect_start;
-            std::chrono::duration<double> pose_time = getPose_end - getPose_start;
-
-            std::cout << "FrameTime: " << get_frame_time.count() << " s\n";
-            std::cout << "MemoryTime: " << memory_time.count() << " s\n";
-            std::cout << "DetectTime: " << detect_time.count() << " s\n";
-            std::cout << "PoseTime: " << pose_time.count() << " s\n";
-            std::cout << "FPS: " << 1.0 / frame_time.count() << "\n";
-            std::cout << "\n";
-
-            finalToSend.clear();
-            cv::imshow("frame", frame); 
-            if (cv::waitKey(10)==27)
-                break;
-            processing_done = true;
+    while (true) {
+        std::unique_lock<std::mutex> lock(frame_queue_mutex);
+        frame_queue_cond.wait(lock, []{ return !frame_queue.empty(); });
+        
+        cv::Mat local_frame = frame_queue.front();  
+        frame_queue.pop();  
+        lock.unlock();  /
+        
+        if (local_frame.empty()) {
+            break;
         }
-    }
+
+       
+        cv::cvtColor(local_frame, img_rgba8, cv::COLOR_BGR2RGBA);
+
+        auto frame_end = std::chrono::steady_clock::now();
+        
+        auto memory_start = std::chrono::steady_clock::now();
+
+        const cudaError_t cuda_error =
+            cudaMemcpyAsync(impl_->input_image_buffer, (uchar4 *)img_rgba8.ptr<unsigned char>(0),
+            impl_->input_image_buffer_size, cudaMemcpyHostToDevice, impl_->main_stream);
+
+        if (cuda_error != cudaSuccess) {
+            throw std::runtime_error(
+                    "Could not memcpy to device CUDA memory (error code " +
+                    std::to_string(cuda_error) + ")");
+        }
+        auto memory_end = std::chrono::steady_clock::now();
+        
+        
+        auto detect_start = std::chrono::steady_clock::now();
+        uint32_t numDetections;
+        const int error = nvAprilTagsDetect(
+                impl_->april_tags_handle, &(impl_->input_image), impl_->tags.data(),
+                &numDetections, impl_->max_tags, impl_->main_stream);
+        if (error != 0) {
+            throw std::runtime_error("Failed to run AprilTags detector (error code " +
+                                    std::to_string(error) + ")");
+        }
+        auto detect_end = std::chrono::steady_clock::now();
+        
+        auto getPose_start = std::chrono::steady_clock::now();
+        std::cout << numDetections;
+        std::cout << std::endl;
+
+        for (int i = 0; i < numDetections; i++) {
+            std::vector<cv::Point2d> imagePts;
+            const nvAprilTagsID_t &detection = impl_->tags[i];
+            for (auto corner : detection.corners) {
+                imagePts.push_back(cv::Point2d(corner.x, corner.y));
+                cv::circle(frame, cv::Point(corner.x, corner.y), 3, cv::Scalar(255, 255, 255));
+            }
+            finalToSend.push_back(getPose(imagePts, detection.id));
+        }
+
+        for (const auto &pose : finalToSend) {
+            std::cout << "Tag ID: " << pose[7] << " | Pose: ";
+            for (size_t i = 0; i < 7; i++) {
+                std::cout << pose[i] << " ";
+            }
+            std::cout << std::endl;
+        }
+        
+        roborioSender(finalToSend, roborioSock);
+        auto getPose_end = std::chrono::steady_clock::now();
+
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> frame_time = end - start;
+        
+        std::chrono::duration<double> get_frame_time = frame_end - start;
+        std::chrono::duration<double> memory_time = memory_end - memory_start;
+        std::chrono::duration<double> detect_time = detect_end - detect_start;
+        std::chrono::duration<double> pose_time = getPose_end - getPose_start;
+
+        std::cout << "FrameTime: " << get_frame_time.count() << " s\n";
+        std::cout << "MemoryTime: " << memory_time.count() << " s\n";
+        std::cout << "DetectTime: " << detect_time.count() << " s\n";
+        std::cout << "PoseTime: " << pose_time.count() << " s\n";
+        std::cout << "FPS: " << 1.0 / frame_time.count() << "\n";
+        std::cout << "\n";
+
+        finalToSend.clear();
+        cv::imshow("frame", frame); 
+        if (cv::waitKey(10)==27)
+            break;
+        
+        }
 }
 
 int main() {
